bf3ad6108bbed45bbe5bb2fbeba42f5d55492475
Nitesh <nitesh02@nagarro.com>
2024-09-19 12:24:23 +0530

    updated test cases

--- src/@/sidebar/ui/school-view-component/tests/school-coverage-layer.test.tsx
+++ src/@/sidebar/ui/school-view-component/tests/school-coverage-layer.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { getAllByText, render } from '@testing-library/react';
 import SchoolCoverageLayer from '../school-coverage-layer/school-coverage-layer';
 import { router } from '~/core/routes';
 import { testWrapper } from '~/tests/jest-wrapper';
@@ -15,7 +15,7 @@ describe('SchoolCoverageLayer', () => {
 
   test('check SchoolCoverageLayer with multiple school ids', () => {
     router.navigate(`/map/schools?country=AI&school_ids=12,13`);
-    const { getByText } = render(testWrapper(<SchoolCoverageLayer />));
-    expect(getByText('Data layer selection', { exact: false })).toBeInTheDocument();
+    const { getAllByText } = render(testWrapper(<SchoolCoverageLayer />));
+    expect(getAllByText(/Data layer selection/i)).toBeTruthy();
   });
 });
