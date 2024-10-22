0a13b190f2027fafc22d1c4b8902502e1cf3c96a
vipulbhavsar94 <vipulbpl@gmail.com>
2024-10-22 18:08:43 +0530

    school details fix

--- src/@/sidebar/config/school-information-config.ts
+++ src/@/sidebar/config/school-information-config.ts
@@ -5,36 +5,47 @@ export type StatisticConfig = {
   key: keyof SchoolStatsType['statistics'];
   label: string;
   icon?: React.ComponentType;
+  group: string;
 };
 
+export const groupOrder = [
+  'Technology',
+  'Other',
+  'Teachers',
+  'Students',
+  'Infrastructure',
+  'General',
+  'Connectivity',
+];
+
 const allStatistics: StatisticConfig[] = [
-  { key: 'num_students', label: 'Number of students' },
-  { key: 'num_teachers', label: 'Number of teachers' },
-  { key: 'num_classroom', label: 'Number of classrooms' },
-  { key: 'num_latrines', label: 'Number of latrines' },
-  { key: 'running_water', label: 'Running water' },
-  { key: 'electricity_availability', label: 'Electricity' },
-  { key: 'computer_lab', label: 'Computer lab' },
-  { key: 'num_computers', label: 'Number of computers' },
-  { key: 'connectivity', label: 'Connectivity' },
-  { key: 'connectivity_status', label: 'Connectivity status' },
-  { key: 'connectivity_type', label: 'Connectivity type' },
-  { key: 'connectivity_speed', label: 'Connectivity speed' },
-  { key: 'connectivity_latency', label: 'Connectivity latency' },
-  { key: 'coverage_availability', label: 'Coverage availability' },
-  { key: 'coverage_type', label: 'Coverage type' },
-  { key: 'connectivity_govt', label: 'Government connectivity' },
-  { key: 'computer_availability', label: 'Computer availability' },
-  { key: 'num_students_girls', label: 'Number of female students' },
-  { key: 'num_students_boys', label: 'Number of male students' },
-  { key: 'num_students_other', label: 'Number of other students' },
-  { key: 'num_teachers_female', label: 'Number of female teachers' },
-  { key: 'num_teachers_male', label: 'Number of male teachers' },
-  { key: 'teachers_trained', label: 'Number of trained teachers' },
-  { key: 'sustainable_business_model', label: 'Sustainable business model' },
-  { key: 'device_availability', label: 'Device availability' },
-  { key: 'num_tablets', label: 'Number of tablets' },
-  { key: 'num_robotic_equipment', label: 'Number of robotic equipment' }
+  { key: 'num_students', label: 'Number of students', group: 'General' },
+  { key: 'num_teachers', label: 'Number of teachers', group: 'General' },
+  { key: 'num_classroom', label: 'Number of classrooms', group: 'Infrastructure' },
+  { key: 'num_latrines', label: 'Number of latrines', group: 'Infrastructure' },
+  { key: 'running_water', label: 'Running water', group: 'Infrastructure' },
+  { key: 'electricity_availability', label: 'Electricity', group: 'Infrastructure' },
+  { key: 'computer_lab', label: 'Computer lab', group: 'Technology' },
+  { key: 'num_computers', label: 'Number of computers', group: 'Technology' },
+  { key: 'connectivity', label: 'Connectivity', group: 'Connectivity' },
+  { key: 'connectivity_status', label: 'Connectivity status', group: 'Connectivity' },
+  { key: 'connectivity_type', label: 'Connectivity type', group: 'Connectivity' },
+  { key: 'connectivity_speed', label: 'Connectivity speed', group: 'Connectivity' },
+  { key: 'connectivity_latency', label: 'Connectivity latency', group: 'Connectivity' },
+  { key: 'coverage_availability', label: 'Coverage availability', group: 'Connectivity' },
+  { key: 'coverage_type', label: 'Coverage type', group: 'Connectivity' },
+  { key: 'connectivity_govt', label: 'Government connectivity', group: 'Connectivity' },
+  { key: 'computer_availability', label: 'Computer availability', group: 'Technology' },
+  { key: 'num_students_girls', label: 'Number of female students', group: 'Students' },
+  { key: 'num_students_boys', label: 'Number of male students', group: 'Students' },
+  { key: 'num_students_other', label: 'Number of other students', group: 'Students' },
+  { key: 'num_teachers_female', label: 'Number of female teachers', group: 'Teachers' },
+  { key: 'num_teachers_male', label: 'Number of male teachers', group: 'Teachers' },
+  { key: 'teachers_trained', label: 'Number of trained teachers', group: 'Teachers' },
+  { key: 'sustainable_business_model', label: 'Sustainable business model', group: 'Other' },
+  { key: 'device_availability', label: 'Device availability', group: 'Technology' },
+  { key: 'num_robotic_equipment', label: 'Number of robotic equipment', group: 'Technology' },
+  { key: 'num_tablets', label: 'Number of tablets', group: 'Technology' },
 ];
 
 const defaultStatistics: (keyof SchoolStatsType['statistics'])[] = [
@@ -58,3 +69,5 @@ export const getStatisticsConfig = async (countryCode: number): Promise<Statisti
 
   return allStatistics.filter(stat => enabledKeys.includes(stat.key));
 };
+
+
