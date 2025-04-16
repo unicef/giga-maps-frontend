35732ea39bbb4c83e7ec8ec76e0fbdd02312ceab
Nitesh <nitesh02@nagarro.com>
2025-04-16 12:49:46 +0530

    temporary url and default category

--- src/@/api-docs/ui/components/api-info/api-info.view.tsx
+++ src/@/api-docs/ui/components/api-info/api-info.view.tsx
@@ -14,11 +14,12 @@ const ApiInfo = () => {
   const iframeRef = useRef<null | HTMLIFrameElement>(null);
   const documentationURL = useMemo(() => {
     if (exploreApiData?.code === 'DAILY_CHECK_APP' && activeApiKeyData?.length > 0) {
-      const category = activeApiKeyData[0]?.api_category_code;
-      const url = new URL(exploreApiData?.documentation_url)
+      const category = activeApiKeyData[0]?.api_category_code || 'public';
+      const url = new URL(exploreApiData?.documentation_url?.includes('.html') ? 'https://uni-ooi-giga-meter-backend-dev.azurewebsites.net/api?gigaUI=true' : exploreApiData?.documentation_url) // TEMP setup documentation URL
       const token = encodeURIComponent(currentApiKey);
       url.pathname += `/${category}`;
-      return url.href + `?token=${token}`;
+      url.searchParams.append('token', token);
+      return url.href;
     }
     return exploreApiData?.documentation_url
   }, [exploreApiData, activeApiKeyData])
