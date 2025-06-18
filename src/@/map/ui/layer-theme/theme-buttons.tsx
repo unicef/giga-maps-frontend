9eb9b1a957a3121ce7423c3987edb228836a288b
Nitesh <nitesh02@nagarro.com>
2025-06-18 18:29:27 +0530

    fix ui changes

--- src/@/map/ui/layer-theme/theme-buttons.tsx
+++ src/@/map/ui/layer-theme/theme-buttons.tsx
@@ -1,4 +1,4 @@
-import { Layers } from '@carbon/icons-react'
+import { Settings } from '@carbon/icons-react'
 import { IconButton } from '@carbon/react'
 import { useStore } from 'effector-react';
 import { useTheme } from 'styled-components';
@@ -8,11 +8,9 @@ import ClickAnywhere from '~/@/sidebar/ui/common-components/click-anywhere';
 
 import { ActiveButtonWrapper } from '../legend-info/legend-button.style';
 import { ThemeWrapper } from './theme-button.style'
-import { themeLayerBg } from './theme-layer-bg';
 import ThemePopup from './theme-popup';
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import SettingIcon from '~/assets/images/setting.svg'
 
 
 const ThemeButtons = () => {
@@ -32,18 +30,15 @@ const ThemeButtons = () => {
   const sidebarHeight = useStore($sidebarHeight)
   return (
     <>
-      <ThemeWrapper className="theme-wrapper-popuptheme-wrapper-popup" $zIndex={isOpen ? 0 : 1} $bottom={sidebarHeight}>
+      <ThemeWrapper className="theme-wrapper-popup" $zIndex={isOpen ? 0 : 1} $bottom={sidebarHeight}>
         <ThemePopup open={isOpen} setOpen={onShowThemeLayer}>
           <ActiveButtonWrapper >
-            <IconButton 
+            <IconButton
               align="left"
               size="sm"
-              className="button_placement"
               label={t("theme-layers")}
               onClick={openLayerTheme}>
-                <span style={{ marginTop: '-10px', display: 'inline-block' }}>
-              <SettingIcon/>
-              </span>
+              <Settings />
             </IconButton>
           </ActiveButtonWrapper>
         </ThemePopup>
