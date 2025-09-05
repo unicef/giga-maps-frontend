ed8d3a1f812f3804bfbb40f6b1052b81ffa23ba3
Nitesh <nitesh02@nagarro.com>
2025-09-05 15:49:35 +0530

    revert aboutus changes

--- src/@/about-giga-map/about.model.ts
+++ src/@/about-giga-map/about.model.ts
@@ -65,12 +65,12 @@ export const $schoolMappedData = sample({
   fn: ({ globalStats, connectivityStats }) => {
     return [{
       type: 'schools',
-      value: `<span data-title="${globalStats?.schools_connected ?? 0}">${formatNumber(globalStats?.schools_connected ?? 0)}</span>`,
+      value: `${formatNumber(globalStats?.schools_connected ?? 0)}/6M`,
       chart: null
     },
     {
       type: 'school-connectivity',
-      value: `<span data-title="${globalStats?.schools_with_connectivity_status_mapped ?? 0}">${formatNumber((globalStats?.schools_with_connectivity_status_mapped ?? 0))}</span>`,
+      value: formatNumber((globalStats?.schools_with_connectivity_status_mapped ?? 0)),
       chart: {
         categories: [ConnectivityStatusNames.connected, ConnectivityStatusNames.not_connected, /*ConnectivityStatusNames.unknown*/],
         categoryColors: [styleData.good, styleData.bad, styleData.unknown],
@@ -82,7 +82,7 @@ export const $schoolMappedData = sample({
     },
     {
       type: 'connectivity',
-      value: `<span data-title="${connectivityStats?.no_of_schools_measure ?? 0}">${formatNumber(connectivityStats?.no_of_schools_measure ?? 0)}</span>`,
+      value: formatNumber(connectivityStats?.no_of_schools_measure ?? 0),
       chart: {
         categories: [ConnectivityDistributionNames.good, ConnectivityDistributionNames.moderate, ConnectivityDistributionNames.bad, ConnectivityDistributionNames.unknown],
         categoryColors: [styleData.good, styleData.moderate, styleData.bad, styleData.unknown],
