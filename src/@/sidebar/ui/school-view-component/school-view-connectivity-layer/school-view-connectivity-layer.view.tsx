fba1a94c1aee380767b5993887d43a17fb4ac216
Nitesh <nitesh02@nagarro.com>
2025-12-30 10:55:23 +0530

    fix url handling on school and admin

--- src/@/sidebar/ui/school-view-component/school-view-connectivity-layer/school-view-connectivity-layer.view.tsx
+++ src/@/sidebar/ui/school-view-component/school-view-connectivity-layer/school-view-connectivity-layer.view.tsx
@@ -1,9 +1,8 @@
 import { useStore } from 'effector-react';
 
-import { $getSchoolParams } from '~/@/sidebar/init';
 
 import { useTranslation } from 'react-i18next';
-import { $schoolStats } from '~/@/sidebar/sidebar.model';
+import { $getSchoolParams, $schoolStats } from '~/@/sidebar/sidebar.model';
 import CurrentLayerNameIcon from '../../common-components/current-layer-name-Icon';
 import SidebarDublicateSchoolList from '../common/dublicate-school-list-view';
 import MultiSchoolLayerView from '../common/multi-school-layer.view';
