febd6b4707f03792a73dc72b1dd442a2aa68e65f
reanbrenda <mukindiabrenda67@gmail.com>
2026-03-26 15:18:57 +0100

    adding territories to countries

--- src/@/sidebar/ui/breadcrumb/global-bdb.view.tsx
+++ src/@/sidebar/ui/breadcrumb/global-bdb.view.tsx
@@ -18,8 +18,7 @@ const GlobalBDB = () => {
 
   return (<div className="sidebar-worldview-global-indication">
     <div className="sidebar-worldview-global-indication-text-and-contries">
-      <span className="sidebar-worldview-globalText">{t('worldwide')}</span>
-      <span className="sidebar-worldview-contriesText">{`${noOfCounties} ${t('countries')}`}</span>
+      <span className="sidebar-worldview-contriesText">{t('countries-and-territories', { count: noOfCounties })}</span>
     </div>
     {/* <Tooltip align="bottom" label="worldwide information for register countries" className='side-info-panel-infomartion-of-worldwideview'>
       <button>
