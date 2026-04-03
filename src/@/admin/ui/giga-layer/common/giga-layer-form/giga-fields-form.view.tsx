bd2efb9d2bab0f724ac860ec4c0b258d47740b50
Nitesh <nitesh02@nagarro.com>
2026-04-03 12:09:40 +0530

    Revert "fix file upload size"
    This reverts commit c3667bf7437014c1f47e62dec55d213ac958bf98.

--- src/@/admin/ui/giga-layer/common/giga-layer-form/giga-fields-form.view.tsx
+++ src/@/admin/ui/giga-layer/common/giga-layer-form/giga-fields-form.view.tsx
@@ -94,6 +94,7 @@ export default function GigaFields({ isEditMode, isDefaultLayer }: { readonly is
           id="layer-description"
           value={formData.description}
           onChange={(e) => onUdpateGigaLayerForm([e.target.name, e.target.value])}
+          required
           placeholder="Enter layer description"
         />
       </DataLayerNameField>
