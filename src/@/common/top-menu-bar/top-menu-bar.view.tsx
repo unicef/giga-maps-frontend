980e88628c57c7ea468d3acca4d768f6f355096a
Nitesh <nitesh02@nagarro.com>
2026-07-06 17:29:48 +0530

    top header color

--- src/@/common/top-menu-bar/top-menu-bar.view.tsx
+++ src/@/common/top-menu-bar/top-menu-bar.view.tsx
@@ -17,7 +17,7 @@ const TopMenuBar = ({
   const isLight = useStore($theme) === ThemeType.light;
   return (
     <div
-      className="flex w-full items-center justify-between border-b border-white/8 bg-[#161616] text-foreground! [&_.cds--btn--ghost]:!bg-transparent [&_.cds--btn--ghost:hover]:!bg-transparent [&_.cds--btn--ghost:active]:!bg-transparent [&_.cds--btn--ghost:focus]:!bg-transparent [&_.cds--btn--icon-only]:!flex [&_.cds--btn--icon-only]:!min-h-8 [&_.cds--btn--icon-only]:!min-w-8 [&_.cds--btn--icon-only]:!items-center [&_.cds--btn--icon-only]:!justify-center [&_.cds--btn--icon-only]:!p-0 [&_.cds--popover-caret]:!bg-current [&_.cds--tooltip-content]:!bg-current [&_svg]:!fill-[#f4f4f4]"
+      className="flex w-full items-center justify-between border-b border-white/8 text-foreground! [&_.cds--btn--ghost]:!bg-transparent [&_.cds--btn--ghost:hover]:!bg-transparent [&_.cds--btn--ghost:active]:!bg-transparent [&_.cds--btn--ghost:focus]:!bg-transparent [&_.cds--btn--icon-only]:!flex [&_.cds--btn--icon-only]:!min-h-8 [&_.cds--btn--icon-only]:!min-w-8 [&_.cds--btn--icon-only]:!items-center [&_.cds--btn--icon-only]:!justify-center [&_.cds--btn--icon-only]:!p-0 [&_.cds--popover-caret]:!bg-current [&_.cds--tooltip-content]:!bg-current [&_svg]:!fill-[#f4f4f4]"
     >
       <div className="flex h-11 w-full items-center justify-start gap-2 px-3.5! py-0">
         <Tooltip>
