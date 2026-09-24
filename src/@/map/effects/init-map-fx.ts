dee864c4b928d70a9a3b75c47619c53ff950bff3
Nitesh <nitesh02@nagarro.com>
2026-09-24 12:15:02 +0530

    fix init map

--- src/@/map/effects/init-map-fx.ts
+++ src/@/map/effects/init-map-fx.ts
@@ -4,61 +4,51 @@ import mapboxGL from 'mapbox-gl';
 import { defaultCenter, defaultZoom, maxZoom, styleUrls } from '~/@/map/map.constant';
 import { changeMap, onStyleLoaded, onZoomLevelChange, onZoomStateChange } from '~/@/map/map.model';
 import { InitMapOptions } from '~/@/map/map.types';
-import { captureComponentError } from '~/core/sentry';
 import { API_MAPBOX_ACCESS_TOKEN } from '~/env';
-
 let timeout: ReturnType<typeof setTimeout>;
-
 export const initMapFx = createEffect(
   ({ style, container, center, zoom }: InitMapOptions) => {
-    try {
-      mapboxGL.accessToken = API_MAPBOX_ACCESS_TOKEN;
+    mapboxGL.accessToken = API_MAPBOX_ACCESS_TOKEN;
 
-      const map = new mapboxGL.Map({
-        style: styleUrls[style],
-        center: center ?? defaultCenter,
-        zoom: zoom ?? defaultZoom,
-        maxZoom,
-        container,
-      });
+    const map = new mapboxGL.Map({
+      style: styleUrls[style],
+      center: center ?? defaultCenter,
+      zoom: zoom ?? defaultZoom,
+      maxZoom,
+      container,
+    });
+    map.dragRotate.disable();
+    map.touchZoomRotate.disableRotation();
+    map.on('load', () => {
+      changeMap(map);
+    });
 
-      map.on('error', (e) => {
-        if (e?.error) {
-          captureComponentError(e.error, 'MapboxGL', {
-            message: e.error.message,
-            style,
-            zoom,
-          });
-        }
-      });
+    map.on('zoomstart', (e) => {
+      if (e.originalEvent) return;
+      const date = new Date();
+      clearTimeout(timeout);
+      onZoomStateChange('start');
+    });
 
-      map.on('zoom', () => {
-        onZoomLevelChange(Number(map.getZoom().toFixed(2)));
-      });
+    map.on('zoom', () => {
+      onZoomLevelChange(Number(map.getZoom().toFixed(2)));
+    });
 
-      map.on('zoomstart', (e) => {
-        if (e.originalEvent) return;
-        clearTimeout(timeout);
-        onZoomStateChange('start');
-      });
+    // map.on('zoom', () => {
+    //   const date = new Date();
+    //   console.log('zoom', date.getHours(), date.getMinutes(), date.getSeconds(), date.getMilliseconds());
+    // });
 
-      map.on('zoom', () => {
-        onZoomLevelChange(Number(map.getZoom().toFixed(1)));
-      });
+    map.on('zoomend', (e) => {
+      if (e.originalEvent) return;
+      const date = new Date();
+      timeout = setTimeout(() => {
+        onZoomStateChange('end');
+      }, 10);
+    });
 
-      map.on('zoomend', (e) => {
-        if (e.originalEvent) return;
-        timeout = setTimeout(() => {
-          onZoomStateChange('end');
-        }, 10);
-      });
-
-      map.on('styledata', () => {
-        onStyleLoaded();
-      });
-    } catch (error) {
-      captureComponentError(error, 'InitMapFx', { style, center, zoom });
-      throw error;
-    }
+    map.on('styledata', (e) => {
+      onStyleLoaded();
+    });
   }
 );
